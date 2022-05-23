725f5cb004b84399cc193e53a501393848d70b67
Kishore <99469638+dirkishore@users.noreply.github.com>
2022-05-23 12:45:55 +0530

    Add files via upload

---
+++ FibonacciSeries.java
@@ -0,0 +1,19 @@
+package core_java;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int firstNum = 0, secondNum = 1;
+		System.out.print(firstNum + " " + secondNum);
+		int count = 10;
+		for (int elementPosition = 2; elementPosition < count; ++elementPosition) {
+			int sumOfTwoNum = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = sumOfTwoNum;
+			System.out.print(" " + sumOfTwoNum);
+			elementPosition++;
+		}
+
+	}
+
+}
